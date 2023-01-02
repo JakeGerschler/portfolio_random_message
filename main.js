20984c985a150048c896ea055c388825f767f305
Jake <jakeg@gmx.de>
2023-01-02 12:14:36 +0100

    Add luckyAnimal + motivationQoute Array

---
+++ main.js
@@ -0,0 +1,6 @@
+const luckyAnimal = ["Löwe", "Tiger", "Puma", "Jaguar", "Adler", "Hai", "Elefant", "Gorilla"];
+const motivationQoute = ["Fehler sind Verrat am Arbeitgeber", "Quadratmeter sind Hektar für Arme", "Kohlen sind nichts anderes als Diamanten die dem Druck nicht standgehalten haben", "Die Liebe zu einem Menschen ist immer auch ein Kostenfaktor", "Die besten Gedanken entstehen im Kopf", "Übertriff dich selbst, die anderen haben es doch auch geschafft", "Der Löwe ist der Hai unter den Adlern", "Mut rückwärts heißt tun!", "Das lächeln eines Kindes ist soviel weniger Wert als Geld", "Ein schöner Tag ändert nichts an einem verpfuschten Leben"];
+
+function mainMethod(name=""){
+}
+
